66dcf99f131f8c0efc7934fb7aec2754291dffe2
KevinOspina <kevinospina03@gmail.com>
2021-02-18 13:06:15 -0500

    funcionalidad de ### añadida

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule, Router } from '@angular/router'
 //Componentes para rutas
 import {HomeComponent} from './Componentes/Home/home/home.component';
 import {ServicesComponent} from './Componentes/Services/services/services.component';
-import {ErrorComponent} from './Componentes-Extra/Error/error/error.component';
+//import {ErrorComponent} from './Componentes-Extra/Error/error/error.component';
 import {InicioComponent} from './Componentes/Inicio/inicio/inicio.component'
 //Array de rutas
 const appRountes: Routes = [
@@ -14,7 +14,7 @@ const appRountes: Routes = [
   {path: 'home/:nombre/:usuario', component: HomeComponent},
   {path: 'services', component: ServicesComponent},
   {path: 'init', component:InicioComponent},
-  {path: '**', component: ErrorComponent}
+  //{path: '**', component: ErrorComponent}
 ];
 
 
